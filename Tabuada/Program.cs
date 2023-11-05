1cb38db98fbd5bd7673169230cd31d52e3c2c5a6
Alex Cardozo <alex.cardozo1910@hotmail.com>
2023-11-05 17:56:44 -0300

    Adicionar arquivos de projeto.

---
+++ Tabuada/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int multiplicador = 1; multiplicador <= 10; multiplicador++)
+            {
+                Console.WriteLine($"Tabuada do {multiplicador}:");
+
+                for (int multiplicando = 1; multiplicando <= 10; multiplicando++)
+                {
+                    int resultado = multiplicador * multiplicando;
+                    Console.WriteLine($"{multiplicador} x {multiplicando} = {resultado}");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
